9c85ef4a3e8dc20bcbd2ea1df3e97040c49fdcb0
Carlos Espinosa <carloespinosa88@gmail.com>
2024-06-18 14:39:32 -0500

    Add some files and reorder the folders

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,10 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-export const AuthScreen = () => {
+export const LoginScreen = () => {
   return (
     <View>
-      <Text>AuthScreen!!!</Text>
+      <Text>LoginScreen</Text>
     </View>
   );
 };
